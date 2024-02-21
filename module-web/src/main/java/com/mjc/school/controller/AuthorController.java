1d77530dc5995ed4ef6f7bf689a13e1896a2ea8a
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2024-02-21 16:18:33 +0300

    Delete module-web/src/main/java/com/mjc/school/controller/AuthorController.java

--- module-web/src/main/java/com/mjc/school/controller/AuthorController.java
+++
@@ -1,9 +0,0 @@
-package com.mjc.school.controller;
-
-import com.mjc.school.service.dto.AuthorDtoRequest;
-import com.mjc.school.service.dto.AuthorDtoResponse;
-import org.springframework.http.ResponseEntity;
-
-public interface AuthorController extends BaseController<AuthorDtoRequest, AuthorDtoResponse, Long> {
-    ResponseEntity<AuthorDtoResponse> readByNewsId(Long newsId);
-}
