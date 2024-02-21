a003acf1a600035c92730e6a98a380578b2ce795
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2024-02-21 13:52:06 +0300

    Delete module-service directory

--- module-service/src/main/java/com/mjc/school/service/annotations/Valid.java
+++
@@ -1,9 +0,0 @@
-package com.mjc.school.service.annotations;
-
-import java.lang.annotation.*;
-
-@Documented
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Valid {
-}
