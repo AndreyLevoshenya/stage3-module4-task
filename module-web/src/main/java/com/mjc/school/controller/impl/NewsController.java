d2473d15e74d2d350e2646782066d0831d47345a
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2024-02-23 14:12:42 +0300

    Add files via upload

---
+++ module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
@@ -0,0 +1,70 @@
+package com.mjc.school.controller.impl;
+
+import com.mjc.school.controller.BaseController;
+import com.mjc.school.service.NewsService;
+import com.mjc.school.service.dto.NewsDtoRequest;
+import com.mjc.school.service.dto.NewsDtoResponse;
+import com.mjc.school.service.dto.ParametersDtoRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/news")
+public class NewsController implements BaseController<NewsDtoRequest, NewsDtoResponse, Long> {
+    private final NewsService newsService;
+
+    @Autowired
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @Override
+    @GetMapping(params = "version=1")
+    public ResponseEntity<List<NewsDtoResponse>> readAll(
+            @RequestParam(required = false, defaultValue = "1") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(name = "sort_by", required = false, defaultValue = "id::asc") String sortBy) {
+
+        return new ResponseEntity<>(newsService.readAll(page, size, sortBy), OK);
+    }
+
+    @Override
+    @GetMapping(value = "/{id:\\d+}", params = "version=1")
+    public ResponseEntity<NewsDtoResponse> readById(@PathVariable Long id) {
+        return new ResponseEntity<>(newsService.readById(id), OK);
+    }
+
+    @Override
+    @PostMapping(params = "version=1")
+    public ResponseEntity<NewsDtoResponse> create(@RequestBody NewsDtoRequest createRequest) {
+        return new ResponseEntity<>(newsService.create(createRequest), CREATED);
+    }
+
+    @Override
+    @PutMapping(params = "version=1")
+    public ResponseEntity<NewsDtoResponse> update(@RequestBody NewsDtoRequest updateRequest) {
+        return new ResponseEntity<>(newsService.update(updateRequest), OK);
+    }
+
+    @Override
+    @PatchMapping(params = "version=1")
+    public ResponseEntity<NewsDtoResponse> patch(@RequestBody NewsDtoRequest updateRequest) {
+        return new ResponseEntity<>(newsService.patch(updateRequest), OK);
+    }
+
+    @Override
+    @DeleteMapping(value = "/{id:\\d+}", params = "version=1")
+    public ResponseEntity<Boolean> deleteById(@PathVariable Long id) {
+        return new ResponseEntity<>(newsService.deleteById(id), NO_CONTENT);
+    }
+
+    @GetMapping(value = "/byParams", params = "version=1")
+    public ResponseEntity<List<NewsDtoResponse>> readByParams(@RequestBody ParametersDtoRequest parametersDtoRequest) {
+        return new ResponseEntity<>(newsService.readByParams(parametersDtoRequest), OK);
+    }
+}
---
+++ module-web/src/main/java/com/mjc/school/controller/impl/TagController.java
@@ -0,0 +1,69 @@
+package com.mjc.school.controller.impl;
+
+import com.mjc.school.controller.BaseController;
+import com.mjc.school.service.TagService;
+import com.mjc.school.service.dto.TagDtoRequest;
+import com.mjc.school.service.dto.TagDtoResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/tags")
+public class TagController implements BaseController<TagDtoRequest, TagDtoResponse, Long> {
+    private final TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @Override
+    @GetMapping(params = "version=1")
+    public ResponseEntity<List<TagDtoResponse>> readAll(
+            @RequestParam(required = false, defaultValue = "1") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(name = "sort_by", required = false, defaultValue = "id::asc") String sortBy
+    ) {
+        return new ResponseEntity<>(tagService.readAll(page, size, sortBy), OK);
+    }
+
+    @Override
+    @GetMapping(value = "/{id:\\d+}", params = "version=1")
+    public ResponseEntity<TagDtoResponse> readById(@PathVariable Long id) {
+        return new ResponseEntity<>(tagService.readById(id), OK);
+    }
+
+    @Override
+    @PostMapping(params = "version=1")
+    public ResponseEntity<TagDtoResponse> create(@RequestBody TagDtoRequest createRequest) {
+        return new ResponseEntity<>(tagService.create(createRequest), CREATED);
+    }
+
+    @Override
+    @PutMapping(params = "version=1")
+    public ResponseEntity<TagDtoResponse> update(@RequestBody TagDtoRequest updateRequest) {
+        return new ResponseEntity<>(tagService.update(updateRequest), OK);
+    }
+
+    @Override
+    @PatchMapping(params = "version=1")
+    public ResponseEntity<TagDtoResponse> patch(@RequestBody TagDtoRequest updateRequest) {
+        return new ResponseEntity<>(tagService.patch(updateRequest), OK);
+    }
+
+    @Override
+    @DeleteMapping(value = "/{id:\\d+}", params = "version=1")
+    public ResponseEntity<Boolean> deleteById(@PathVariable Long id) {
+        return new ResponseEntity<>(tagService.deleteById(id), NO_CONTENT);
+    }
+
+    @GetMapping(value = "/get/{newsId:\\d+}", params = "version=1")
+    public ResponseEntity<List<TagDtoResponse>> readByNewsId(@PathVariable Long newsId) {
+        return new ResponseEntity<>(tagService.readByNewsId(newsId), OK);
+    }
+}
